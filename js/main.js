35b747881eb77f7feb9255d9d6860921aa558abe
iqbalpb01 <iqbalbasheer646@gmail.com>
2020-01-21 20:40:34 +0530

    transtion speed & new transition added

--- js/main.js
+++ js/main.js
@@ -418,7 +418,7 @@ anime.timeline({loop: true})
     scaleX: [0,1],
     opacity: [0.5,1],
     easing: "easeInOutExpo",
-    duration: 900
+    duration: 2500
   }).add({
     targets: '.ml14 .letter',
     opacity: [0,1],
@@ -426,11 +426,56 @@ anime.timeline({loop: true})
     translateZ: 0,
     scaleX: [0.3, 1],
     easing: "easeOutExpo",
-    duration: 800,
+    duration: 3000,
     offset: '-=600',
     delay: (el, i) => 150 + 25 * i
   }).add({
     targets: '.ml14',
+    opacity: .6,
+    duration: 5000,
+    easing: "easeOutExpo",
+    delay: 1200
+  });
+  
+  
+  
+  
+  
+  anime.timeline({loop: true})
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  }).add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 3000,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
